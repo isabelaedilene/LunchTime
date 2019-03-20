44e1155ea02c4e8f206ac673300fe58a12e8f65c
Ronan Felipe <ronan.nascimento@sga.pucminas.br>
2019-03-20 19:29:49 -0300

    React Home

---
+++ LunchTime/client/style/colors.js
@@ -0,0 +1,21 @@
+import Color from 'color';
+
+export default {
+    white: '#FFF',
+    lighter: '#EEE',
+    light: '#DDD',
+    regular: '#999',
+    dark: '#666',
+    darker: '#333',
+    black: '#000',
+
+    primary: '#DA552F',
+    primaryDark: Color('#DA552F').darken(0.1),
+    secundary: '#fafafa',
+    success: '#9DCA83',
+    danger: '#E37A7A',
+
+    transparent: 'transparent',
+    darkTransparent: 'rgba(0, 0, 0, 0.6)',
+    whiteTransparent: 'rgba(255, 255, 255, 0.3)',
+};
