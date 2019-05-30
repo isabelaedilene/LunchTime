b4c3def98d983383836abcb700061444147fa2da
Ronan Felipe <ronan.nascimento@sga.pucminas.br>
2019-05-30 00:52:23 -0300

    error fixed with raw query

--- LunchTime/app/controllers/pedidoController.js
+++ LunchTime/app/controllers/pedidoController.js
@@ -1,16 +1,17 @@
 const Sequelize = require('sequelize');
 const models = require('../models');
+const mysql = require('mysql');
 
-exports.post = (req, res, next) => {
-    models.Pedidos.create({
-        IdProduto: req.body.IdProduto,
-        idCliente_fk: req.body.idCliente_fk,
-        idRestaurante_fk: req.body.idRestaurante_fk,
-        valorCompra: req.body.valorCompra,
-        horarioChegada: req.body.horarioChegada,
-        statusPedido: req.body.statusPedido
-    }).then((result) => res.json(result))
-};
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'lunchDB',
+    port: '3306',
+});
+
+connection.connect();
 
 exports.get = async (req, res, next) => {
     models.Pedidos.findAll({
@@ -19,5 +20,28 @@ exports.get = async (req, res, next) => {
         res.json({
             pedidos: pedidos
         });
+    });
+};
+
+exports.post = (req, res) => {
+    let pedido = {
+        IdProduto: req.body.IdProduto,
+        idCliente_fk: req.body.idCliente_fk,
+        idRestaurante_fk: req.body.idRestaurante_fk,
+        valorCompra: req.body.valorCompra,
+        horarioChegada: req.body.horarioChegada,
+        statusPedido: req.body.statusPedido,
+        createdAt: '2019-04-25 20:12:59',
+        updatedAt: '2019-04-25 20:12:59',
+    };
+
+    let query = "INSERT INTO `pedidos` " +
+        "(IdProduto, idCliente_fk, idRestaurante_fk, valorCompra, horarioChegada, statusPedido, createdAt, updatedAt )" +
+        "VALUES" +
+        "(?, ?, ?, ?, ?, ?, ?, ?)";
+
+    connection.query(query, [pedido.IdProduto, pedido.idCliente_fk, pedido.idRestaurante_fk, pedido.valorCompra, pedido.horarioChegada, pedido.statusPedido, pedido.createdAt, pedido.updatedAt], function (err, result) {
+        if (err) throw err;
+        console.log("Value inserted ")
     });
 };
