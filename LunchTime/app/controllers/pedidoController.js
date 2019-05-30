a663a684ea193786c7eaeb83241ce0b8898c0e29
Ronan Felipe <ronan.nascimento@sga.pucminas.br>
2019-05-29 23:17:34 -0300

    post pedido (error)

--- LunchTime/app/controllers/pedidoController.js
+++ LunchTime/app/controllers/pedidoController.js
@@ -3,18 +3,18 @@ const models = require('../models');
 
 exports.post = (req, res, next) => {
     models.Pedidos.create({
-        idCliente_fk: req.body.idCliente,
-        idRestaurante_fk: req.body.idRestaurante,
+        IdProduto: req.body.IdProduto,
+        idCliente_fk: req.body.idCliente_fk,
+        idRestaurante_fk: req.body.idRestaurante_fk,
         valorCompra: req.body.valorCompra,
         horarioChegada: req.body.horarioChegada,
-        statusPedido: req.body.statusPedido,
-        IdProduto: req.body.IdProduto
+        statusPedido: req.body.statusPedido
     }).then((result) => res.json(result))
 };
 
 exports.get = async (req, res, next) => {
     models.Pedidos.findAll({
-        include: [{all: true}],
+        attributes: ['idPedidos', 'idCliente_fk', 'idRestaurante_fk', 'valorCompra', 'horarioChegada', 'statusPedido', 'IdProduto']
     }).then(function (pedidos) {
         res.json({
             pedidos: pedidos
